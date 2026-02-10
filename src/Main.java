c501fa3f42f7daaac5127661413642edaa437728
Nicolas Butarbutar <nicolaszuliaaan@gmail.com>
2026-02-10 09:59:39 +0700

    Submit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,95 @@
-import java.util.*;
+// Nama : NICOLAS J GRACE BUTARBUTAR
+// NIM : 12S24038
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // TODO: implement solution
+
+        String soal = sc.next(); // Soal1 / Soal2 / Soal3 / Soal4 / Soal5
+
+        if (soal.equals("Soal1")) {
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            boolean overflow = false;
+
+            if (a > 0 && b > 0 && a > Integer.MAX_VALUE - b) {
+                overflow = true;
+            } else if (a < 0 && b < 0 && a < Integer.MIN_VALUE - b) {
+                overflow = true;
+            }
+
+            if (overflow) {
+                System.out.println("OVERFLOW");
+            } else {
+                System.out.println(a + b);
+            }
+
+        } else if (soal.equals("Soal2")) {
+
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+
+        // Penjumlahan menggunakan float
+        float fx = (float) x;
+        float fy = (float) y;
+        float floatSum = fx + fy;
+
+        // Penjumlahan menggunakan double
+        double doubleSum = x + y;
+
+        // Selisih absolut
+        double diff = Math.abs(floatSum - doubleSum);
+
+        // Output 6 digit di belakang koma
+        System.out.printf("%.6f%n", diff);
+
+        } else if (soal.equals("Soal3")) {
+
+            int N = sc.nextInt();
+
+            Integer a = N;
+            Integer b = a;
+
+            a = a + 1;
+
+            System.out.println("==: " + (a == b));
+            System.out.println("equals: " + a.equals(b));
+
+        } else if (soal.equals("Soal4")) {
+
+            String S = sc.next();
+
+            String a = S;
+            String b = new String(S);
+
+            a = a + "X";
+
+            System.out.println("==: " + (a == b));
+            System.out.println("equals: " + a.equals(b));
+
+        } else if (soal.equals("Soal5")) {
+
+            String intStr = sc.next();
+            String doubleStr = sc.next();
+            String boolStr = sc.next();
+
+            int i = Integer.parseInt(intStr);
+            double d = Double.parseDouble(doubleStr);
+            boolean b = Boolean.parseBoolean(boolStr);
+
+            double result = i * d;
+
+            if (!b) {
+                result *= -1;
+            }
+
+            System.out.printf("%.2f%n", result);
+        }
+
+        sc.close();
     }
 }
